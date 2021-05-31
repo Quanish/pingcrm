c5ed46e12d2641c67791295d1e60f08d46265d32
Quanish <kakuanish2@gmail.com>
2021-05-31 15:38:12 +0600

    31.05.2021

---
+++ database/migrations/2021_05_31_092201_crate_subtasks_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CrateSubtasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('subtask',function(Blueprint $table){
+            $table->id('id');
+            $table->integer('task_id');
+            $table->string('title');
+            $table->string('description');
+            $table->timestamp('start')->useCurrent();
+            $table->timestamp('deadline')->useCurrent();
+            $table->tinyInteger('status');
+            $table->timestamps();
+            $table->softDeletes($column = 'deleted_at', $precision = 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('subtasks');
+    }
+}
